f5aaed13d091a768a033716cfc8e1878de6e71db
Rakeshsingh5 <156055487+Rakeshsingh5@users.noreply.github.com>
2026-03-26 05:19:35 +0000

    done

--- mid/for_each.js
+++ mid/for_each.js
@@ -20,5 +20,33 @@ const mycode =  [
  ]
 
  mycode.forEach( (item) => {
-    console.log(item.name1);
+    // console.log(item.name1);
  })
+
+
+ const vec = [1 ,2,3,4,5,6,6,7,8];
+ const myarr =  [];
+
+ vec.forEach((num)=> {
+    if(num >4)myarr.push(num);
+
+ })
+
+//  console.log(myarr);
+
+
+ const newnum= vec.filter( (num) => { return num>4})  //if put scope {} always use return;
+//  console.log(newnum);
+
+ const newnum1= vec.filter( (num) =>   num<4)  
+//  console.log(newnum1);
+
+const total = vec.reduce((acc,cur) =>  {
+    // console.log(`acc : ${acc}`);
+    return acc+cur } ,0);
+// console.log(total); 
+
+const tot = vec.reduce((acc , curr) => acc+curr ,0);  
+console.log(tot); 
+
+ 
